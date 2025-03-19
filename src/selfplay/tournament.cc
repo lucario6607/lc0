3e92579bcb5347e9e310d741da31a4cff0a0edcd
lucario6607 <bowlingkings@icloud.com>
2025-03-19 16:10:04 -0500

    Update tournament.cc

--- src/selfplay/tournament.cc
+++ src/selfplay/tournament.cc
@@ -118,7 +118,7 @@ void SelfPlayTournament::PopulateOptions(OptionsParser* options) {
 
   options->Add<BoolOption>(kShareTreesId) = true;
   options->Add<IntOption>(kTotalGamesId, -2, 999999) = -1;
-  options->Add<IntOption>(kParallelGamesId, 1, 256) = 8;
+  options->Add<IntOption>(kParallelGamesId, 1, 1024) = 8;
   options->Add<IntOption>(kPlayoutsId, -1, 999999999) = -1;
   options->Add<IntOption>(kVisitsId, -1, 999999999) = -1;
   options->Add<IntOption>(kTimeMsId, -1, 999999999) = -1;
